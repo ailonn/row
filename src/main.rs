d50cf97a279d12f65768304ea07eae866ec15019
ailonn lightner <ailonn.lightner@protonmail.com>
2022-07-01 22:35:13 +0200

    add bevy system to exit on escape input

--- src/main.rs
+++ src/main.rs
@@ -47,5 +47,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(HelloPlugin)
+        .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
