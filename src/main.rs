462182491ccbc483591b6b9f175088af17bc6d35
ailonn lightner <ailonn.lightner@protonmail.com>
2022-07-03 00:04:02 +0200

    move the tutorial code to a different file and clean main file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use bevy::prelude::{App, DefaultPlugins, ClearColor, Color};
-mod tutorial;
-use crate::tutorial::tutorial::HelloPlugin;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
